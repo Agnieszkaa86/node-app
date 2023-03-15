fd242934795019ef765038af2289a0ff0a699868
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-15 11:32:25 +0100

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+ const express = require("express");
+const { createUser } = require("../controllers/users");
+
+ const router = express.Router();
+//  //nie trzymamy logiki w routes/users.js
+router.post("/", (req, res) => {
+    try {
+        const { name, age } = req.body;
+        const user = createUser(name, age);
+       return res.status(201).json(user);
+     } catch {
+        return res.status(500).send("something went wrong");     }
+ });
+ module.exxports = router;
